c763031cdb88e417a7381e9db873e75765fb1a4f
Omar Sainz <SQL_Slammer.exe@live.de>
2015-08-13 10:52:56 -0500

    Footer added but is not visible, problems with models view

---
+++ client/app/components/footer.directive.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('rainbowApp')
+    .directive('acmeFooter', acmeFooter);
+
+  /** @ngInject */
+  function acmeFooter() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/footer.html',
+      scope: {
+          creationDate: '='
+      },
+      controller: NavbarController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function FooterController(moment) {
+      var vm = this;
+
+      // "vm.creation" is avaible by directive option "bindToController: true"
+      vm.relativeDate = moment(vm.creationDate).fromNow();
+    }
+  }
+
+})();
